49b5c2e869794c43dc1a14de4144083bb89f87c6
Unknown <nvenkov1@jhu.edu>
2018-02-03 17:26:12 -0500

    Main
    Edit output

--- main.cpp
+++ main.cpp
@@ -13,29 +13,33 @@ int main(int argc, char** argv) {
     int ny = atoi(argv[2]);
     int x0 = atoi(argv[3]);
     int y0 = atoi(argv[4]);
-    string fname = argv[5];
+    int verb = atoi(argv[5]);
+    string fname = argv[6];
     
     NpcfTools npcf(fname);
-    int verb=2;
     int error = npcf.read_file(nx,ny,x0,y0,verb);
-    if (!error) {
-        npcf.get_s2();     
-        printf("\ns2(0,0) = %f, s2(1,0) = %f, s2(2,0) = %f\n",npcf.s2(0,0),npcf.s2(1,0),npcf.s2(2,0));
-        printf("\ns2(0,0) = %f, s2(0,1) = %f, s2(0,2) = %f\n",npcf.s2(0,0),npcf.s2(0,1),npcf.s2(0,2));
+    if (!error) {   
+        if (verb>1) {
+            npcf.get_s2();  
+            printf("\ns2(0,0) = %f, s2(1,0) = %f, s2(2,0) = %f\n",npcf.s2(0,0),npcf.s2(1,0),npcf.s2(2,0));
+            printf("\ns2(0,0) = %f, s2(0,1) = %f, s2(0,2) = %f\n",npcf.s2(0,0),npcf.s2(0,1),npcf.s2(0,2));
+        }
         
         npcf.get_s3();     
-        printf("\ns3(0,0,0,0) = %f, s3(1,0,0,0) = %f, s3(2,0,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(1,0,0,0),npcf.s3(2,0,0,0));
-        printf("\ns3(0,0,0,0) = %f, s3(0,1,0,0) = %f, s3(0,2,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,1,0,0),npcf.s3(0,2,0,0));
-        printf("\ns3(0,0,0,0) = %f, s3(0,0,1,0) = %f, s3(0,0,2,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,0,1,0),npcf.s3(0,0,2,0));
-        printf("\ns3(0,0,0,0) = %f, s3(0,0,0,1) = %f, s3(0,0,0,2) = %f\n\n",npcf.s3(0,0,0,0),npcf.s3(0,0,0,1),npcf.s3(0,0,0,2));
-        
-        printf("\ns2(0,0) = %f, s2(-1,0) = %f, s2(-2,0) = %f\n",npcf.s2(0,0),npcf.s2(-1,0),npcf.s2(-2,0));
-        printf("\ns2(0,0) = %f, s2(0,-1) = %f, s2(0,-2) = %f\n",npcf.s2(0,0),npcf.s2(0,-1),npcf.s2(0,-2));
-        
-        printf("\ns3(0,0,0,0) = %f, s3(-1,0,0,0) = %f, s3(-2,0,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(-1,0,0,0),npcf.s3(-2,0,0,0));
-        printf("\ns3(0,0,0,0) = %f, s3(0,-1,0,0) = %f, s3(0,-2,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,-1,0,0),npcf.s3(0,-2,0,0));
-        printf("\ns3(0,0,0,0) = %f, s3(0,0,-1,0) = %f, s3(0,0,-2,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,0,-1,0),npcf.s3(0,0,-2,0));
-        printf("\ns3(0,0,0,0) = %f, s3(0,0,0,-1) = %f, s3(0,0,0,-2) = %f\n\n",npcf.s3(0,0,0,0),npcf.s3(0,0,0,-1),npcf.s3(0,0,0,-2));
+        if (verb>1) {
+            printf("\ns3(0,0,0,0) = %f, s3(1,0,0,0) = %f, s3(2,0,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(1,0,0,0),npcf.s3(2,0,0,0));
+            printf("\ns3(0,0,0,0) = %f, s3(0,1,0,0) = %f, s3(0,2,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,1,0,0),npcf.s3(0,2,0,0));
+            printf("\ns3(0,0,0,0) = %f, s3(0,0,1,0) = %f, s3(0,0,2,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,0,1,0),npcf.s3(0,0,2,0));
+            printf("\ns3(0,0,0,0) = %f, s3(0,0,0,1) = %f, s3(0,0,0,2) = %f\n\n",npcf.s3(0,0,0,0),npcf.s3(0,0,0,1),npcf.s3(0,0,0,2));
+
+            printf("\ns2(0,0) = %f, s2(-1,0) = %f, s2(-2,0) = %f\n",npcf.s2(0,0),npcf.s2(-1,0),npcf.s2(-2,0));
+            printf("\ns2(0,0) = %f, s2(0,-1) = %f, s2(0,-2) = %f\n",npcf.s2(0,0),npcf.s2(0,-1),npcf.s2(0,-2));
+
+            printf("\ns3(0,0,0,0) = %f, s3(-1,0,0,0) = %f, s3(-2,0,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(-1,0,0,0),npcf.s3(-2,0,0,0));
+            printf("\ns3(0,0,0,0) = %f, s3(0,-1,0,0) = %f, s3(0,-2,0,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,-1,0,0),npcf.s3(0,-2,0,0));
+            printf("\ns3(0,0,0,0) = %f, s3(0,0,-1,0) = %f, s3(0,0,-2,0) = %f\n",npcf.s3(0,0,0,0),npcf.s3(0,0,-1,0),npcf.s3(0,0,-2,0));
+            printf("\ns3(0,0,0,0) = %f, s3(0,0,0,-1) = %f, s3(0,0,0,-2) = %f\n\n",npcf.s3(0,0,0,0),npcf.s3(0,0,0,-1),npcf.s3(0,0,0,-2));            
+        }
     }
     return 0;
 }
